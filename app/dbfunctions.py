31ab6419973588ec5b5c3335604294bbbb365965
wli15 <61180367+wli15@users.noreply.github.com>
2021-01-07 23:48:46 -0500

    finalized database functions

--- app/dbfunctions.py
+++ app/dbfunctions.py
@@ -4,101 +4,90 @@ DB_FILE="userinfo.db"
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
-c.execute("CREATE TABLE IF NOT EXISTS userInfo (Username TEXT, Password TEXT, id INTEGER PRIMARY KEY)") #for register info
+def createTables():
+	c.execute("CREATE TABLE IF NOT EXISTS userInfo (Username TEXT, Password TEXT, id INTEGER PRIMARY KEY)")
+	c.execute("CREATE TABLE IF NOT EXISTS Uentries (id INTEGER, title TEXT, entry TEXT)")
+	c.execute("CREATE TABLE IF NOT EXISTS Ublogs (id INTEGER, title TEXT)")
 
-c.execute("CREATE TABLE IF NOT EXISTS Ublurb (id INTEGER, UserBlurbs TEXT)")	#for blurb to be posted on userpage
 
-c.execute("CREATE TABLE IF NOT EXISTS Udraft (id INTEGER, Drafts TEXT)")	#for unfinished blogs
-
-c.execute("CREATE TABLE IF NOT EXISTS Upublished (id INTEGER, Published TEXT)")	#for published blogs to be posted
-
-def insert_login_data(userN, passW, id):     #when given data, it inerts into the database/create it
+def insertUserData(userN, passW, id):     #when given data, it inerts into the database/create it
 	insert = "INSERT INTO userInfo (Username, Password, id) VALUES (?, ?, ?);"
 	data = (userN, passW, id)
 	c.execute(insert, data)
 
-def verify_login(username, password):
-	accounts_db = c.execute("SELECT * FROM userInfo")
-	for row in accounts_db:
-		userArray = row
-		if(userArray[0] == userN):		#suppose to look through every row of the userInfo table and see if the the userN exist
-			if(userArray[1] == passW):
+def verifyLogin(username, password):	#gives user and pass to check if the info match
+	userN=username
+	passW=password
+	c.execute("SELECT * FROM userInfo")
+	data = c.fetchall()
+	for row in data:
+		userArray=row
+		if(userArray[0]==userN):
+			print("correct user")
+			if(userArray[1]==passW):
+				print("correct password")
 				return True
+			else:
+				print("wrong pass")
+				return False
 	return False
 
-def publish_draft(userid):	#will move the entire draft to the Upublished database based on userid
-	uid=userid
-	c.execute('SELECT * FROM Udraft WHERE id=?', (uid,))
-	data= c.fetchall()
-	data = (userid, data[0][1])
-	insert = "INSERT INTO Upublished (id, Published) VALUES (?,?);"			#move the entire draft to Upublished table
-	c.execute(insert, data)
-	c.execute("DELETE FROM Udraft WHERE id = ?", (uid,))
-
-def save_draft(userid, userdraft):
-	c.execute("DELETE FROM Udraft WHERE id = 123")		#deletes any old draft to be replaced with new one
-	insert = "INSERT INTO Udraft (id, Drafts) VALUES (?,?);"
-	data = (userid, userdraft)
-	c.execute(insert,data)
-
-def create_blog(userid, blog):
-	data = (userid, blog)
-	insert = "INSERT INTO Upublished (id, Published) Values (?,?);"
+def createBlog(userID, title):
+	data=(userID, title)
+	insert = "INSERT INTO Ublogs (id, title) VALUES (?, ?);"
 	c.execute(insert, data)
 
-def spit_blog(userid):			#feed userid to get all blogs that have same uid
-	username = userid
-	c.execute('SELECT * FROM Upublished WHERE id=?', (username,))
+def createEntry(userID, title, entry):
+	data=(userID, title, entry)
+	insert = "INSERT INTO Uentries (id, title, entry) VALUES (?, ?, ?);"
+	c.execute(insert, data)
+
+def editEntry(userID, title, revisedEntry):
+	user=userID
+	entryTitle=title
+	c.execute('DELETE FROM Uentries WHERE id=? AND title=?', (user, entryTitle)) 
+	newEntry=revisedEntry
+	createEntry(user, entryTitle, newEntry)
+def getBlogTitle(userID):
+	user=userID
+	c.execute('SELECT * FROM Ublogs WHERE id=?', (user,))
+	data=c.fetchall()
+	print(data[0][1])
+	return data[0][1]
+def getEntries(userID):
+	user=userID
+	c.execute('SELECT * FROM Uentries WHERE id=?', (user,))
+	data=c.fetchall()
+	return data
+	#bigStr=""
+	#i=0
+	#for row in data:
+	#	bigStr += data[i][1]
+	#	bigStr += "-"
+	#	bigStr += data[i][2]
+	#	bigStr += "\n"
+	#	i+=1
+	#print(bigStr)
+	#return bigStr
+def getId(userN):
+	user = userN
+	c.execute('SELECT * FROM userinfo WHERE Username=?', (user,))
+	uId=c.fetchall()
+	print(uId[0][2])
+	return uId[0][2]
+def checkUser(userN):
+	user=userN
+	c.execute("SELECT * FROM userInfo")
 	data = c.fetchall()
-	i=0
 	for row in data:
-		blog = data[i][1]
-		print(blog)
-		i+=1
+		userArray=row
+		if(userArray[0]==user):
+			print("existing user")
+			return True
+	print("no user")
+	return False
 
-
-print("-------------this is a test----------------")
-insert_login_data("william", "hi", 12) 		#actually passes html cookies to here to be given to the previous function
-insert_login_data("dog", "cat", 23)
-
-def verify_login(userN, passW):
-	accounts_db = c.execute("SELECT * FROM userInfo")
-	for row in accounts_db:
-		userArray = row
-		if(userArray[0] == userN):		#suppose to look through every row of the userInfo table and see if the the userN exist
-			if(userArray[1] == passW):
-				print("worked")				#correct userN and passW pair
-			else:						#real userN but wrong password
-				print("didn't work")
-
-verify_login("dog", "cat")					#should return worked
-
-accounts_db = c.execute("SELECT * FROM userInfo")		#looks through userInfo row by row and creates a csvfile with each row in the userInfo table
-with open('account.csv', 'w') as f:
-	csv_writer = csv.writer(f)
-	for row in accounts_db:
-		userArray = row
-		csv_writer.writerow(userArray)
-
-print("-----------------------------------")
-
-save_draft(123,"The question is in a way meaningless, she knows, but one must ask.")
-save_draft(23,"During the first part of your life, you only become aware of happiness once you have lost it.")
-save_draft(1,"However, it had never occurred to me to contest this law, nor to imagine disobeying it")
-publish_blog(1, "Christ, he thinks, by my age I ought to know")
-publish_draft(23)
-publish_blog(1, "But just as I didn not want to resent my kids")
-spit_blog(1)
-draft_db = c.execute("SELECT * FROM Udraft")			#should show Udraft that are saved in csv file drafts.csv
-with open('draft.csv', 'w') as f:
-	csv_writer = csv.writer(f)
-	for row in draft_db:
-		draftArray = row
-		csv_writer.writerow(draftArray)
-
-published_db = c.execute("SELECT * FROM Upublished")		#should show Upublished table in csv file published.csv
-with open('published.csv', 'w') as f:
-	csv_writer = csv.writer(f)
-	for row in published_db:
-		pubArray = row
-		csv_writer.writerow(pubArray)
+def getBlog():			
+	c.execute('SELECT * FROM Ublogs')
+	blog=c.fetchall()
+	return blog
